eecfd2a51ebd30f42ae064fcfe6a6d935e670a83
ChrisSperber <christoph.sperber.neuro@gmx.de>
2025-11-08 23:55:36 +0100

    Updated config and requirements

--- src/brain_stats_tools/config.py
+++ src/brain_stats_tools/config.py
@@ -12,7 +12,12 @@ NOT_APPLICABLE = "Not_applicable"
 PATIENT = "patient"
 CONTROL = "control"
 
-MRI_PROCESSING_DIR = (
-    Path(__file__).parents[3] / "metachromatic_leukodystrophy_mri_processing"
+PROJECTS_DIR = Path(__file__).parents[3]
+OUTPUT_METRICS_DIR = PROJECTS_DIR / "MRI_output_metrics"
+CH2_EXAMPLE_PIPELINE_DIR = TEMPORARY_DATA_DIR / "ch2_pipeline_demo"
+
+MRI_METRICS_VOLUMETRY_LONG_CSV = (
+    OUTPUT_METRICS_DIR / "mri_outcome_metrics_volumetric.csv"
 )
-OUTPUT_METRICS_DIR = MRI_PROCESSING_DIR / "MRI_output_metrics"
+MRI_METRICS_FA_LONG_CSV = OUTPUT_METRICS_DIR / "mri_outcome_metrics_FA.csv"
+MRI_METRICS_MD_LONG_CSV = OUTPUT_METRICS_DIR / "mri_outcome_metrics_MD.csv"
